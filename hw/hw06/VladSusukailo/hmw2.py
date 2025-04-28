2598dcb21e37a4646b7722b35208eba26061ba28
vladikbarabaye <brawlerkot336@gmail.com>
2025-04-28 23:52:40 +0300

    added hw

---
+++ hw/hw06/VladSusukailo/hmw2.py
@@ -0,0 +1,8 @@
+login = ""
+while   login != "First":
+    lohin = input("Введіть логін: ")
+    if lohin == "First":
+        print("Привіт!")
+        break
+    else:
+        print("Ви ввели неправильний логін")

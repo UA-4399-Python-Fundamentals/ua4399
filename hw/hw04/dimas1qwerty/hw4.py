73a179fe19cc373b63d81c8c979e599a3fc7df06
dimas1qwerty <dmytro.klymchuk.kb.2023@lpnu.ua>
2025-04-29 22:48:47 +0300

    add homework 05

--- hw/hw04/dimas1qwerty/hw4.py
+++ hw/hw04/dimas1qwerty/hw4.py
@@ -1,4 +1,3 @@
-
 celsius = float(input("Enter the temperature in Celsius: "))
     
 if celsius < -273.15:
@@ -6,5 +5,4 @@ if celsius < -273.15:
 else:
     fahrenheit = (celsius * 9/5) + 32
     print(f"{celsius}°C is equivalent to {round(fahrenheit)}°F")
-
-
+    

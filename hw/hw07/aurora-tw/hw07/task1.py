896e94d81caccef693f03fd2d327f0ee5b90971b
aurora <animasaurora@gmail.com>
2025-04-30 18:41:41 +0100

    complete hw07

---
+++ hw/hw07/aurora-tw/hw07/task1.py
@@ -0,0 +1,8 @@
+def largest_num(num1,num2):
+    """
+    Returns the largest number from two given
+    Args:
+        num1 (int): The first number
+        num2 (int): The second number
+    """
+    return num1 if num1>num2 else num2

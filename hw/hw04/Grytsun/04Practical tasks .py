37541ec166702bbc462753b4c75435aeaca0cb9a
Grytsun <grytsunmaryna@gmail.com>
2025-04-30 22:13:19 +0300

    added hw05 Grytsun

--- hw/hw04/Grytsun/04Practical tasks .py
+++
@@ -1,9 +0,0 @@
-# Temperature Converter
-temp_cels = float(input("Enter the temperature in Celsius: "))
-
-25# Check if the temperature is below absolute zero
-if float(temp_cels) < -273.15:
-        print("Error: Temperature below absolute zero (-273.15°C)")
-else:
-        fahrenheit = (temp_cels * 9/5) + 32
-        print (f"{temp_cels}°C is equivalent to {fahrenheit}°F")

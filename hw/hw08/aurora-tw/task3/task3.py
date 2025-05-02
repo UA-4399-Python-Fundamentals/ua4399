b03072eac40691a88aded0167d2f9debc4877b94
aurora <animasaurora@gmail.com>
2025-05-02 09:58:39 +0100

    fix error in task 3

--- hw/hw08/aurora-tw/task3/task3.py
+++ hw/hw08/aurora-tw/task3/task3.py
@@ -8,6 +8,8 @@ def init():
     """
 
     figure = input("Enter figure to calculate area: rectangle, triangle or circle: ")
+    if figure not in VALS:
+        return f"The figure {figure} is not supported"
     first_num = input(f"Please enter the {VALS[figure][0]} ")
     second_num = input(f"Please enter the {VALS[figure][1]} ") if not figure == "circle" else "0"
     result = calculator(figure, first_num, second_num)

ecfa74ce083f8b0157cce0d76a0649a1d51c0d78
Olya Petrenko <opyashchik160987@gmail.com>
2025-05-02 15:33:56 +0300

    task 2 and task 3

--- hw/hw05/Olya945/hw05.py
+++ hw/hw05/Olya945/hw05.py
@@ -11,3 +11,50 @@ numbers = list(map(int, input_string.split()))
 # Перетворюємо список цілих чисел у дробові числа та виводимо результат
 float_numbers = list(map(float, numbers))
 print("Список дробових чисел:", float_numbers)
+
+
+#second task
+
+print("\nsecond task\n")
+
+# Вводимо число n
+n = int(input("Введіть ціле число n: "))
+
+# Початкові числа послідовності
+a, b = 0, 1
+    
+# Виводимо перше число
+print(a, end=' ')
+    
+# Поки наступне число не перевищує n
+while b <= n:
+    print(b, end=' ')
+    # Обчислюємо наступне число Фібоначчі
+    a, b = b, a + b
+
+print(f"Числа Фібоначчі до {n}:")
+
+
+#third task
+print("\nthird task\n")
+
+def factorial(n):
+     
+    if n == 0 or n == 1:
+        return 1
+    
+    if n < 0:
+        return "Факторіал не визначений для від'ємних чисел"
+    
+    else:
+        return n * factorial(n - 1)
+
+# Вводимо число n
+n = int(input("Введіть ціле число n: "))
+
+# Обчислюємо факторіал
+result = factorial(n)
+
+# Виводимо результат
+print(f"Факторіал числа {n} дорівнює {result}")
+

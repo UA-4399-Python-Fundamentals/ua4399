96875937cb9899b09822bdb68eab996a3bc8a3d8
vladikbarabaye <brawlerkot336@gmail.com>
2025-05-04 01:30:10 +0300

    Added hw

---
+++ hw/hw07/VladSusukailo/homework1.py
@@ -0,0 +1,5 @@
+def max_of_two(a, b):
+    return a if a > b else b
+num1 = (input("Введіть перше число: "))
+num2 = (input("Введіть друге число: "))
+print("Найбільше число є:", max_of_two(num1,num2))

869adc1d0af799e8a5ff036f205369f53f52d434
dimas1qwerty <dmytro.klymchuk.kb.2023@lpnu.ua>
2025-05-07 00:48:47 +0300

    add homework 06

--- hw/hw05/dimas1qwerty/hw05.py
+++ hw/hw05/dimas1qwerty/hw05.py
@@ -6,7 +6,6 @@ for num in int_list:
     float_list.append(float(num))
 
 print(float_list)
-# Результат: [1.0, 2.0, 3.0, 4.0, 5.0]
 
 # Task 2
 n = int(input("Enter a number: "))
@@ -15,8 +14,8 @@ a, b = 0, 1
 while a <= n:
     print(a, end=" ")
     a, b = b, a + b
-# Наприклад, при n = 10 виведе: 0 1 1 2 3 5 8
 print("/n")
+
 # Task 3
 num = int(input("Enter a number: "))
 factorial = 1
@@ -25,4 +24,3 @@ for i in range(1, num + 1):
     factorial *= i
 
 print(f"{num}! = {factorial}")
-# Наприклад, при num = 4 виведе: 4! = 24

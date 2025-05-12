8b3e6b22440a5bb776ae797e7b42641ec04b2391
marynam-1290 <maryna.mykhailyk99@gmail.com>
2025-05-12 20:53:43 +0300

    moved my tasks to correct folder hw08/marynam-1290

---
+++ hw/hw10/marynam-1290/practical_task10.1.py
@@ -0,0 +1,7 @@
+# Task1 Create a polygon class a and a rectangle class that inherits from the polygon class and finds the square of rectangle
+
+
+class Polygon:
+    pass
+
+def __init__(num_sides, side):

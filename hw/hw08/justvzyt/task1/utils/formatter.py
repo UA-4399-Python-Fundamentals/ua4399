3f20cf286fe179ee309d64474540ba36d9eac9ce
Volodymyr Zakolodnyi <zakolodnuyvova@gmail.com>
2025-05-12 16:21:22 +0300

    Added task1, task2, task3 folders with corresponding files.

---
+++ hw/hw08/justvzyt/task1/utils/formatter.py
@@ -0,0 +1,5 @@
+def format_string():
+    print("Function format_string executed.")
+
+def side_function():
+    print("utils: This function should not be imported since it is absent in the __all__ file.")
---
+++ hw/hw08/justvzyt/task3/figures/rectangle.py
@@ -0,0 +1,2 @@
+def area_rectangle(a, b):
+    print(f"Rectangle area with sides {a}, {b} is {a*b}")

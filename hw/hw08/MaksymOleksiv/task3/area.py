c4443769460b28621d4722ce16d316bc6a8ef41b
MaksymOleksiv <maksymoleksiv07@gmail.com>
2025-05-13 14:40:12 +0300

    added hw08

---
+++ hw/hw08/MaksymOleksiv/task3/area.py
@@ -0,0 +1,13 @@
+import math
+
+def rectangle_area(length, width):
+    """Calculate the area of a rectangle."""
+    return length * width
+
+def circle_area(radius):
+    """Calculate the area of a circle."""
+    return math.pi * radius ** 2
+
+def triangle_area(base, height):
+    """Calculate the area of a triangle."""
+    return 0.5 * base * height

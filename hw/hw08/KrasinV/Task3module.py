f3581c0f263ed66082146553c279ecbc39f5f00a
Viacheslav Krasin <krasinv@ukr.net>
2025-05-18 02:23:54 -0400

    hw08/KrasinV Task3

---
+++ hw/hw08/KrasinV/Task3module.py
@@ -0,0 +1,10 @@
+
+def area_rectangular(a,b):
+    return f"Area of rectangular: {a * b}"
+
+def area_triangle(a,b):
+    return f"Area of triagnle: {1/2* a * b}"
+
+def area_circle(r):
+    import math
+    return f"Area of circle: { math.pi * math.pow(r,2)}"
